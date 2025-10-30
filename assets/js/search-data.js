f3ed8f9b03486bc139e095c2434a11c378cb2bf5
jgroschwitz <jonasg@coli.uni-saarland.de>
2025-10-30 11:39:29 +0000

    Deploying to gh-pages from @ natasha-korotkova/natasha-korotkova.github.io@bd05c0f213611afa3e7cef90fb96cfed45222cf9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -405,16 +405,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-i-am-starting-an-nwo-xs-project-the-grammar-of-opinion",
+          title: 'I am starting an NWO XS project “The grammar of opinion”.',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
